57fae10b2376f67bfd40de1c9d8010cafc25defd
Osmig_Torres <torresosmig@gmail.com>
2024-10-08 14:27:01 -0700

    Bed Time Stories exercise

---
+++ src/main/java/Mod01/com/pluralsight/BedTimeStories.java
@@ -0,0 +1,46 @@
+package Mod01.com.pluralsight;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BedTimeStories {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void read_file(String file) throws FileNotFoundException {
+      FileInputStream read = new FileInputStream(file);
+      Scanner scanner = new Scanner(read);
+      String input;
+      int line_counter = 1;
+      System.out.println("\n-------------------------------------- " + file + " --------------------------------------\n");
+      while(scanner.hasNextLine()){
+          input = scanner.nextLine();
+          System.out.println(line_counter + ". " + input);
+          line_counter++;
+      }
+      scanner.close();
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("\n\nPlease choose a story to read: \n1. goldilocks \n2. hansel and gretel \n3. Mary had a little lamb");
+            int input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    read_file("goldilocks.txt");
+                    break;
+                case 2:
+                    read_file("hansel_and_gretel.txt");
+                    break;
+                case 3:
+                    read_file("mary_had_a_little_lamb.txt");
+                    break;
+            }
+            scanner.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Choice out of bound!");
+        }
+    }
+}
--- src/main/java/Mod01/com/pluralsight/FamousQuotes.java
+++ src/main/java/Mod01/com/pluralsight/FamousQuotes.java
@@ -1,11 +1,11 @@
-package com.pluralsight;
+package Mod01.com.pluralsight;
 
 import java.util.Scanner;
 
 public class FamousQuotes {
     static Scanner scanner = new Scanner(System.in);
     static boolean run = true;
-    static String[] quotes = {"1.Can you goback in time? no!, jump to the future? no! but you can learn from the past and fix your future",
+    private static String[] quotes = {"1.Can you goback in time? no!, jump to the future? no! but you can learn from the past and fix your future",
             "2. If you took the time to breathe when you're stressed you wouldnt be stressed!",
             "3. if life is unfair to everyone, then is life fair?",
             "4. If you could travel anywhere you'd eventually travel there",
@@ -25,7 +25,7 @@ public class FamousQuotes {
 
     public static void random_quote(Scanner scanner, String[] quotes)
     {
-        int random = (int) (Math.random()* 10) + 1; // gives a random number in between 10
+        int random = (int) (Math.random()* 10); // gives a random number in between 10
         System.out.println("Do you want a random quote? (y/n)");
         String choice = scanner.nextLine();
 
@@ -37,22 +37,18 @@ public class FamousQuotes {
             case "n":  // if not wanted random quote ill will execute the list of quotes
                 not_random(scanner, quotes);
         }
-
     }
 
     public static void not_random(Scanner Scanner, String[] quote)
     {
-//        for (String phrase : quotes) // display each quote
-//        {
-//            System.out.println(phrase);
-//        }
         System.out.println("\nPlease select between 1 - 10:"); // takes in user input
         int choice = scanner.nextInt();
         scanner.nextLine();
 
         if (choice > 10 || choice < 1) // throws error in case the input is out of range
         {
-            throw new RuntimeException("Choice out of bounce!");
+            //throw new RuntimeException("Choice out of bounce!");
+            System.out.println("Choice out of bounds!");
         }
 
         switch (choice) { // checks if inpur is the same as a case in list
@@ -89,10 +85,8 @@ public class FamousQuotes {
         }
     }
 
-
         public static void main (String[]args)
         {
-
             do { // it executes the block of code at least once
                 quotes(scanner); // containts the functions which display random or list of quotes
                 System.out.println("Do you want to continue? (y/n)");

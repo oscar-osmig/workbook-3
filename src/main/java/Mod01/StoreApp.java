32eac8627bf469dca8fc702d392e58b86e23e87e
Osmig_Torres <torresosmig@gmail.com>
2024-10-09 16:40:16 -0700

    Added comments on StoreApp.java

--- src/main/java/Mod01/StoreApp.java
+++ src/main/java/Mod01/StoreApp.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class StoreApp {
-    public static Scanner scanner = new Scanner(System.in);
+//    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        ArrayList<Product> inventory = getInventory();
+        ArrayList<Product> inventory = getInventory(); // makes a list type Product and assigns the return of GetInventory()
         //Scanner scanner = new Scanner(System.in);
         System.out.println("We carry the following inventory: ");
         for (int i = 0; i < inventory.size(); i++) {
@@ -18,18 +19,18 @@ public class StoreApp {
 
         }
     }
-    public static ArrayList<Product> getInventory() throws IOException {
-        ArrayList<Product> inventory = new ArrayList<Product>();
+    public static ArrayList<Product> getInventory() throws IOException { // returns a list type Product
+        ArrayList<Product> inventory = new ArrayList<Product>(); // list of data type Product, list name 'inventory'
     // this method loads product objects into inventory
-    //        inventory.add(new Product(123, "phone", 333.3f));
+    //        inventory.add(new Product(123, "phone", 333.3f)); way to add product manually
     // and its details are not shown
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("inventory.csv"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("inventory.csv")); // reads lines in provided file
     //        bufferedReader.readLine();
         String input;
 
-        while((input = bufferedReader.readLine()) != null){
-            String[] product = input.split(Pattern.quote("|"));
-            Product product1 = new Product(Integer.parseInt(product[0]), product[1], Float.parseFloat(product[2]));
+        while((input = bufferedReader.readLine()) != null){ // while the line is not the last in file it continues
+            String[] product = input.split(Pattern.quote("|")); // splits the lines by | and store each element in a string array
+            Product product1 = new Product(Integer.parseInt(product[0]), product[1], Float.parseFloat(product[2])); // make a new product with the parts of the array we made
             inventory.add(product1); // adding new product to the 'inventory' list
         }
         return inventory;
